a6e3a789e102ea3467dce58ee0811b6955010e5d
Miao <tymiao@gwmail.gwu.edu>
2020-08-06 13:03:18 -0400

    cron

--- web_client/views/index.js
+++ web_client/views/index.js
@@ -1,9 +1,5 @@
 import * as configuration from './configuration';
-import * as layouts from './layouts';
-import * as widgets from './widgets';
 
 export {
-    configuration,
-    layouts,
-    widgets
+    configuration
 };
