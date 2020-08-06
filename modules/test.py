1acddc4e12e22eabab19039f449f748985e3a396
Miao <tymiao@gwmail.gwu.edu>
2020-08-06 16:35:01 -0400

    cron

--- modules/test.py
+++ modules/test.py
@@ -1,15 +1,20 @@
 import time
 import os, sys
+import argparse
 
-path = sys.argv[1]
-path = os.path.join(path, 'out.log')
-if os.path.exists(path):
-  os.remove(path)
-for a in range(0,5):
+def main(output):
+  path = os.path.join(output, 'out.log')
+  if os.path.exists(path):
+    os.remove(path)
+  for a in range(0,5):
+    f = open(path, 'a')
+    time.sleep(1)
+    f.write(str(a) + '\n')
+    f.close()
   f = open(path, 'a')
-  time.sleep(1)
-  f.write(str(a) + '\n')
+  f.write('finished \n')
   f.close()
-f = open(path, 'a')
-f.write('finished \n')
-f.close()
+if __name__ == '__main__':
+  parser.add_argument('-o', '--output', help='output.', required=True)
+  kwargs = vars(parser.parse_args())
+  main(**kwargs)
