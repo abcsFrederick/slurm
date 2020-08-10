80ca6226dc82de0b5b832a25cbdca2763ac4dad3
Miao <tymiao@gwmail.gwu.edu>
2020-08-10 15:56:19 -0400

    get slurm

--- server/crontab.sh
+++ server/crontab.sh
@@ -2,6 +2,6 @@
 
 jobinfo=$(/usr/local/bin/squeue -j $1)
 if [[ $jobinfo != *$1* ]]; then
-  echo http://localhost:8888/api/v1/slurm/update?commentId=$2&slurmJobId=$1
-  curl -X POST --header 'Content-Length: 0' http://localhost:8888/api/v1/slurm/update?commentId=$2&slurmJobId=$1
+  echo "http://localhost:8888/api/v1/slurm/update?commentId=$2&slurmJobId=$1"
+  curl -X POST --header 'Content-Length: 0' "http://localhost:8888/api/v1/slurm/update?commentId=$2&slurmJobId=$1"
 fi
