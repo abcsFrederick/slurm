9974fd9b94d36794a10c9c596a54b20b0be4307e
Miao <tymiao@gwmail.gwu.edu>
2020-09-04 17:13:50 -0400

    get slurm

--- server/event_handlers.py
+++ server/event_handlers.py
@@ -4,6 +4,7 @@ import sys, os
 import re
 from crontab import CronTab
 
+from girder import events
 from girder.models.setting import Setting
 from girder.plugins.jobs.constants import JobStatus
 from girder.plugins.jobs.models.job import Job
@@ -84,8 +85,8 @@ mkdir -p {shared_partition_work_directory}/slurm-$SLURM_JOB_NAME.$SLURM_JOB_ID
             events.trigger('cron.watch', {'slurmJobId': slurmJobId})
             job['otherFields']['slurm_info']['slurm_id'] = slurmJobId
             job = Job().save(job)
+            print job
             Job().updateJob(job, status=JobStatus.RUNNING)
-            
         except Exception:
             return 'something wrong during slurm start'
         
